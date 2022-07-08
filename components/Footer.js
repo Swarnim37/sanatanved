aceb37821358cd415c1f8d03e2b2e16ebdfc302b
Swarnim37 <swarnimsharma37@gmail.com>
2022-07-08 12:06:41 +0200

    fixes

--- components/Footer.js
+++ components/Footer.js
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
+import { AiFillFacebook, AiFillTwitterSquare } from "react-icons/ai";
 
 const Footer = () => {
   return (
@@ -57,31 +58,13 @@ const Footer = () => {
                 <a className="text-gray-1000 mr-6">Cookies Policy</a>
               </Link>
               <Link href="https://www.facebook.com/Sanatan-Vedic-Astrology-106720835429910">
-                <a className="text-gray-900">
-                  <svg
-                    fill="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    className="w-5 h-5"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-                  </svg>
+                <a className="text-gray-900 mt-1">
+                  <AiFillFacebook />
                 </a>
               </Link>
               <Link href="https://twitter.com/SanatanAstro3">
-                <a className="ml-3 text-gray-900">
-                  <svg
-                    fill="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    className="w-5 h-5"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-                  </svg>
+                <a className="ml-3 text-gray-900 mt-1 ">
+                  <AiFillTwitterSquare />
                 </a>
               </Link>
             </span>
