eb33e7c05cdc25b7e195a87d55dd8da22d23bc8d
Swarnim37 <swarnimsharma37@gmail.com>
2022-07-13 23:17:38 +0200

    more fixes

--- pages/career.js
+++ pages/career.js
@@ -19,10 +19,12 @@ const Career = () => {
           <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center font-abc">
             <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
               Career Reading
-              <br className="hidden lg:inline-block" />
-              Recorded Audio of 15-30 Minutes.
             </h1>
-            <p className="mb-8 leading-relaxed text-justify">
+
+            <h2 className="title-font sm:text-4xl text-xl mb-4 font-medium text-gray-900">
+              Recorded Audio of 15-30 Minutes.
+            </h2>
+            <p className="mb-8 leading-relaxed m-4 text-justify">
               You can be interested in several different types of careers. The
               chart can show which direction is the most suitable for you, where
               you can achieve success and feel fulfilled. Planetary periods
--- pages/childbirth.js
+++ pages/childbirth.js
@@ -19,10 +19,12 @@ const Childbirth = () => {
           <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
             <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
               Child Birth Reading
-              <br className="hidden lg:inline-block" />
-              Recorded Audio of 15-30 Minutes.
             </h1>
-            <p className="mb-8 leading-relaxed text-justify">
+
+            <h2 className="title-font sm:text-4xl text-xl mb-4 font-medium text-gray-900">
+              Recorded Audio of 15-30 Minutes.
+            </h2>
+            <p className="mb-8 leading-relaxed m-4 text-justify">
               A thorough natal chart as well as related divisional charts
               examination of both parents together with planetary periods can
               show you when the planetary influences are the most auspicious for
